76222a4fea89ba2141d5a5274caa2b273c9fb9c5
Zachary Roof <zroof85@gmail.com>
2017-12-15 07:31:15 -0500

    Updating javascript-babel

--- injection-fundamentals-1/src/execfile.js
+++ injection-fundamentals-1/src/execfile.js
@@ -1,6 +1,10 @@
-var execFile = require('child_process').execFile; 
-var user_defined_url = "example.com/callback";
-var curl = execFile("/usr/bin/curl", [user_defined_url]);
-curl.stdout.on('data', function(data) {
-  console.log(data);
-});
+// [[file:~/repos/sts-tutorials/injection-fundamentals-1/notes.org::EXECFILE][EXECFILE]]
+var test
+  var execFile = require('child_process').execFile;
+  var user_defined_url = "example.com/callback";
+  var curl = execFile("/usr/bin/curl", [user_defined_url]);
+  curl.stdout.on('data', function(data) {
+    console.log(data);
+  });
+// Visit %filename
+// EXECFILE ends here
