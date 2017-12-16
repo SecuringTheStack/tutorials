e7863a1ea6c6a0730bc94af2388fc5ca49d91a37
Zachary Roof <zroof85@gmail.com>
2017-12-16 13:36:01 -0500

    Cleaning up while I work more locally

--- injection-fundamentals-1/src/foo.js
+++
@@ -1,6 +0,0 @@
-var exec = require('child_process').exec;
-var user_defined_url = "example.com/callback";
-var curl = exec("curl " + user_defined_url);
-curl.stdout.on('data', function(data) {
-  console.log(data);
-});
